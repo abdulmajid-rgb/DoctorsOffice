46dd521a555d17491435e352c68c25e2219cdc1a
Abdulmajid-rgb <eabdulmejid@gmail.com>
2020-06-15 17:34:34 -0400

    DoctorsOffice

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,21 @@
+public class DoctorsOffice {
+    public class Main {
+        public static void main(String[] args) {
+
+
+            String patientName = "Stacy Mason";
+            boolean hasInsurance = false;
+            double temp = 102.2;
+            int numVisits = 5;
+
+            System.out.println("Patient Details:");
+            System.out.println("Name: " + patientName)
+            System.out.println("Patient has insurance: " + hasInsurance);
+            System.out.println("Body temprature: ");
+        }
+
+
+        }
+
+
+}
